caa684c22c45f5947786b2fa0d87afd273cf34b6
Hamza <96245936+hamzabely1@users.noreply.github.com>
2023-04-25 19:49:53 +0200

    init page home

--- front/src/connection/Login.tsx
+++ front/src/connection/Login.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Heder from "../components/Heder";
+import NavBar from "../components/NavBar";
 
 
 const Login = () => {
     return (
         <div>
+            <NavBar/>
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign
